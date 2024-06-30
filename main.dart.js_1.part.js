88c0a79fcba412cd0a5c16115d5ec0f059514dc7
pintusingh28 <singhpintu.8089@gmail.com>
2024-07-01 00:58:12 +0530

    Built web
    Branch: main
    Commit: 7b7856f291d2a9e0154a90ba85748b9a2b4a39ca (dirty)
    
    package:peanut 5.0.0

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -2,33 +2,33 @@
 a[b][c]=a[b][c]||[]
 a[b][c].push({p:"main.dart.js_1",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={
-f2(d){return new A.adq(d)},
-pN:function pN(){},
-adq:function adq(d){this.a=d},
-aEp(d){var x="Experience",w="Projects",v="Services"
-return B.aR(["aboutContent",A.f2("Hello, I'm Pintu Singh. I am a self-taught Flutter developer with over 4 years of experience in mobile and web app development. I also have extensive knowledge in Android native development and backend development using Node.js. My passion lies in designing and creating beautiful, user-friendly apps and websites.\n\nI am committed to constantly learning and adapting to new technologies to enrich my skills and expand my knowledge as a developer. Throughout my career, I have been delighted to help my clients and colleagues enhance their digital experiences.\n\nIn addition to my professional expertise, I actively contribute to open source projects, which allows me to give back to the developer community and collaborate with other talented individuals. This involvement not only helps improve the software I work on but also keeps me at the forefront of technological advancements.\n\nBeyond my professional life, I enjoy photography and traveling, which provide me with fresh perspectives and inspiration. Currently, I reside in Surat, Gujarat, India."),"aboutOptionLabel",A.f2("About"),"aboutScreenTitle",A.f2("About"),"appTitle",A.f2("Pintu Singh - Portfolio"),"changeThemeTooltip",A.f2("Change Theme"),"contactOptionLabel",A.f2("Contact"),"contactScreenTitle",A.f2("Contact Me"),"darkThemeLabel",A.f2("Dark"),"experienceOptionLabel",A.f2(x),"experienceScreenTitle",A.f2(x),"introPageDescription",A.f2("A Flutter developer fueled by a passion for crafting beautiful, user-friendly apps and websites."),"lightThemeLabel",A.f2("Light"),"projectsOptionLabel",A.f2(w),"projectsScreenTitle",A.f2(w),"servicesOptionLabel",A.f2(v),"servicesScreenTitle",A.f2(v),"systemThemeLabel",A.f2("System")],y.g,y.a)},
-PQ:function PQ(d){this.a=d},
+f3(d){return new A.adt(d)},
+pO:function pO(){},
+adt:function adt(d){this.a=d},
+aEo(d){var x="Experience",w="Projects",v="Services"
+return B.aR(["aboutContent",A.f3("Hello, I'm Pintu Singh. I am a self-taught Flutter developer with over 4 years of experience in mobile and web app development. I also have extensive knowledge in Android native development and backend development using Node.js. My passion lies in designing and creating beautiful, user-friendly apps and websites.\n\nI am committed to constantly learning and adapting to new technologies to enrich my skills and expand my knowledge as a developer. Throughout my career, I have been delighted to help my clients and colleagues enhance their digital experiences.\n\nIn addition to my professional expertise, I actively contribute to open source projects, which allows me to give back to the developer community and collaborate with other talented individuals. This involvement not only helps improve the software I work on but also keeps me at the forefront of technological advancements.\n\nBeyond my professional life, I enjoy photography and traveling, which provide me with fresh perspectives and inspiration. Currently, I reside in Surat, Gujarat, India."),"aboutOptionLabel",A.f3("About"),"aboutScreenTitle",A.f3("About"),"appTitle",A.f3("Pintu Singh - Portfolio"),"changeThemeTooltip",A.f3("Change Theme"),"contactOptionLabel",A.f3("Contact"),"contactScreenTitle",A.f3("Contact Me"),"darkThemeLabel",A.f3("Dark"),"experienceOptionLabel",A.f3(x),"experienceScreenTitle",A.f3(x),"introPageDescription",A.f3("A Flutter developer fueled by a passion for crafting beautiful, user-friendly apps and websites."),"lightThemeLabel",A.f3("Light"),"projectsOptionLabel",A.f3(w),"projectsScreenTitle",A.f3(w),"servicesOptionLabel",A.f3(v),"servicesScreenTitle",A.f3(v),"systemThemeLabel",A.f3("System")],y.g,y.a)},
+PS:function PS(d){this.a=d},
 aWP(d,e,f){if(d!=="")return d
 return e}}
 B=c[0]
 A=a.updateHolder(c[3],A)
-A.pN.prototype={
-akC(d,e,f,g,h,i){var x=this.a.i(0,A.aWP(f,d,h))
+A.pO.prototype={
+akD(d,e,f,g,h,i){var x=this.a.i(0,A.aWP(f,d,h))
 if(x==null)return d
 else return B.aDA(x,g)},
 i(d,e){return this.a.i(0,e)},
 k(d){return"en"}}
-A.PQ.prototype={}
-var z=a.updateTypes(["b7<f,hW>(@)"])
-A.adq.prototype={
+A.PS.prototype={}
+var z=a.updateTypes(["b7<f,hV>(@)"])
+A.adt.prototype={
 $0(){return this.a},
 $S:35};(function installTearOffs(){var x=a._static_1
-x(A,"aYq","aEp",0)})();(function inheritance(){var x=a.inherit
-x(A.pN,B.v)
-x(A.adq,B.y_)
-x(A.PQ,A.pN)})()
-B.aGX(b.typeUniverse,JSON.parse('{"PQ":{"pN":[]}}'))
-var y={a:B.S("hW"),g:B.S("f")};(function lazyInitializers(){var x=a.lazyFinal
-x($,"b2R","aLY",()=>new A.PQ(A.aEp(A.aYq())))})()};
+x(A,"aYq","aEo",0)})();(function inheritance(){var x=a.inherit
+x(A.pO,B.v)
+x(A.adt,B.y1)
+x(A.PS,A.pO)})()
+B.aGW(b.typeUniverse,JSON.parse('{"PS":{"pO":[]}}'))
+var y={a:B.T("hV"),g:B.T("f")};(function lazyInitializers(){var x=a.lazyFinal
+x($,"b2R","aLX",()=>new A.PS(A.aEo(A.aYq())))})()};
 ((a,b)=>{a[b]=a.current
-a.eventLog.push({p:"main.dart.js_1",e:"endPart",h:b})})($__dart_deferred_initializers__,"0y03rQgn+mIXLJJvOD4DQZFEWVY=");
+a.eventLog.push({p:"main.dart.js_1",e:"endPart",h:b})})($__dart_deferred_initializers__,"O+LcE8rxStyD3+lzFtn2mg6/EQg=");
